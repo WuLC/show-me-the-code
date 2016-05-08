bf7c7ec76db79e57a461cf03db0a69bc25c0fdde
WuLC <liangchaowu5@gmail.com>
2016-05-08 16:57:38 +0800

    Find content and urls of a html page through bs4

--- 0008/test_files/config_a6dec193.js
+++
@@ -1,1 +0,0 @@
-F._setMod("mancard");F._fileMap({"/js/mancard_63d3f81e.js":["skeleton/card","skeleton/model","skeleton/card_collection","skeleton/card_showLog","skeleton/presenter","skeleton/card_tab","setting/card_setting","subscribe/parabola","subscribe/subscribe"],"/css/card_min_e8bcf60d.css":["card_setting.css","card_menu.css"]});F._useConfig=true;
--- 0008/test_files/copyrightk.js
+++
@@ -1,9 +0,0 @@
-<!--
-	
-	cpright_html="<TABLE id=\"copyright_table\" name=\"copyright_table\" border=\"0\" cellSpacing=\"0\" cellPadding=\"0\" width=\"1000\"><TBODY><TR><TD bgColor=#333333 height=4></TD></TR><TR><TD bgColor=#cccccc height=4></TD></TR><TR><TD style=\"COLOR: #000000; FONT-SIZE: 12px; TEXT-DECORATION: none\" bgColor=#ffffff height=80 align=middle>Copyright ? 2000 - 2011 XINHUANET.com　All Rights Reserved. 　制作单位：新华网<BR>版权所有 新华网</TD></TR></TBODY></TABLE>";
-
-	
-	document.write(cpright_html);
-	document.writeln("<script type='text/javascript'>try{window.clearInterval(time1);}catch(e){} time1 = window.setInterval('clear_top1(\"copyright_table\")', 500); function clear_top1(name) { 	if(document.readyState!='complete') 		return; 			window.clearInterval(time1); 		listTable_top = document.getElementsByName(name);		if(listTable_top!=null && listTable_top.length > 1)		{ 		for(i=listTable_top.length-1; i!=0; i--)				{			 			_element = listTable_top[i];	       			_parentElement = _element.parentNode;    	     			if(_parentElement)			 			{  				_parentElement.removeChild(_element);        				}				}		}  }	</script>");
-
-//-->
